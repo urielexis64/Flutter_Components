1aebfdec10af6c5e18b1f126e065fb948a995416
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-29 22:39:05 -0700

    dynamic ListTiles from a json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/src/providers/menu_provider.dart';
+import 'package:widgets/src/utils/icon_util_string.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,19 +14,33 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-    return ListView(
-      children: _itemsList(),
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _itemsList(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _itemsList() {
-    return [
-      ListTile(title: Text('adsasd')),
-      Divider(),
-      ListTile(title: Text('adsasd')),
-      Divider(),
-      ListTile(title: Text('adsasd')),
-      Divider(),
-    ];
+  List<Widget> _itemsList(List<dynamic> data) {
+    final List<Widget> options = [];
+
+    data.forEach((element) {
+      final tempWidget = ListTile(
+        title: Text(element['text']),
+        leading: getIcon(element['icon']),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {},
+      );
+      options..add(tempWidget)..add(Divider());
+    });
+
+    return options;
   }
 }
