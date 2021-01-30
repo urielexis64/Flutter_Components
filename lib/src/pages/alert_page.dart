7b84b87a6ab241e9483d7b2705d2e0caee41c976
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-29 23:07:15 -0700

    AlertPage

---
+++ lib/src/pages/alert_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  const AlertPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alert Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.accessible_forward_outlined),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
