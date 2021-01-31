3b9e4bd367900243e744814230ef43a133fe044c
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-31 15:44:42 -0700

    Checkbox and Switch

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -7,6 +7,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _currentValue = 200.0;
+  bool _blockCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,12 @@ class _SliderPageState extends State<SliderPage> {
       body: Container(
         padding: EdgeInsets.only(top: 30),
         child: Column(
-          children: [_createSlider(), Expanded(child: _createImage())],
+          children: [
+            _createSlider(),
+            _createCheckbox(),
+            _createSwitch(),
+            Expanded(child: _createImage())
+          ],
         ),
       ),
     );
@@ -25,17 +31,20 @@ class _SliderPageState extends State<SliderPage> {
 
   Widget _createSlider() {
     return Slider(
-        max: 350,
-        min: 100,
-        activeColor: Colors.indigoAccent,
-        label: 'Image size',
-        value: _currentValue,
-        /* divisions: 10, */
-        onChanged: (value) {
-          setState(() {
-            _currentValue = value;
-          });
-        });
+      max: 350,
+      min: 100,
+      activeColor: Colors.indigoAccent,
+      label: 'Image size',
+      value: _currentValue,
+      /* divisions: 10, */
+      onChanged: _blockCheck
+          ? null
+          : (value) {
+              setState(() {
+                _currentValue = value;
+              });
+            },
+    );
   }
 
   Widget _createImage() {
@@ -47,5 +56,29 @@ class _SliderPageState extends State<SliderPage> {
           'https://static.wikia.nocookie.net/zelda/images/2/26/Link_Artwork_LAR.png/revision/latest?cb=20191015020845&path-prefix=es'),
       placeholder: AssetImage('assets/loadingImage.gif'),
     );
+  }
+
+  Widget _createCheckbox() {
+    return CheckboxListTile(
+        title: Text('Block Slider'),
+        activeColor: Colors.indigoAccent,
+        value: _blockCheck,
+        onChanged: (value) {
+          setState(() {
+            _blockCheck = value;
+          });
+        });
+  }
+
+  Widget _createSwitch() {
+    return SwitchListTile(
+        title: Text('Block Slider'),
+        activeColor: Colors.indigoAccent,
+        value: _blockCheck,
+        onChanged: (value) {
+          setState(() {
+            _blockCheck = value;
+          });
+        });
   }
 }
