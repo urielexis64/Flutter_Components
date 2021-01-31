e4852665349397c920f64ac97b5fb343ddf031ee
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-31 15:16:36 -0700

    Slider page

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:widgets/src/pages/avatar_page.dart';
 import 'package:widgets/src/pages/card_page.dart';
 import 'package:widgets/src/pages/home_page.dart';
 import 'package:widgets/src/pages/input_page.dart';
+import 'package:widgets/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -14,5 +15,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     "card": (BuildContext context) => CardPage(),
     "animatedContainer": (BuildContext context) => AnimatedContainerPage(),
     "inputs": (BuildContext context) => InputPage(),
+    "slider": (BuildContext context) => SliderPage(),
   };
 }
