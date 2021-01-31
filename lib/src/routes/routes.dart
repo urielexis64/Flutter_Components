dca5f705725ddda102e9114e6fdb2c7f95e958c4
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-30 22:00:14 -0700

    routes dart file

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:widgets/src/pages/alert_page.dart';
+import 'package:widgets/src/pages/avatar_page.dart';
+import 'package:widgets/src/pages/card_page.dart';
+import 'package:widgets/src/pages/home_page.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return <String, WidgetBuilder>{
+    "home": (BuildContext context) => HomePage(),
+    "alert": (BuildContext context) => AlertPage(),
+    "avatar": (BuildContext context) => AvatarPage(),
+    "card": (BuildContext context) => CardPage(),
+  };
+}
